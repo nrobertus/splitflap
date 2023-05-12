09e5d83a97b41eb1badc5c0753d68c5d4d3abd8f
nrobertus <nathan.robertus@gmail.com>
2023-05-12 16:41:00 -0600

    More housing updates

--- firmware/stepper_motor/stepper_motor.ino
+++ firmware/stepper_motor/stepper_motor.ino
@@ -2,23 +2,23 @@
 #include <Stepper.h>
 
 // Defines the number of steps per rotation
-const int stepsPerRevolution = 2038;
+const int stepsPerRevolution = 2048;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
 Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
-int stepsPerLetter = stepsPerRevolution / 40;
+int stepsPerLetter = -53;
 
 void setup() {
-    // Nothing to do (Stepper Library sets pins as outputs)
+    myStepper.setSpeed(10);
+}
+
+void moveLetters(int letters) {
+  myStepper.step(letters * stepsPerLetter);
 }
 
 void loop() {
-  delay(5000);
+  delay(500);
   // Rotate CCW quickly at 10 RPM
-  myStepper.setSpeed(5);
-  myStepper.step(-15 * stepsPerLetter);
-  delay(100000);
+  moveLetters(1);
 }
-
-// 37.5 x 72.5 mm
