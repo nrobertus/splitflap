3472b16cd528407de55328a293d71a76d7d21e51
nrobertus <nathan.robertus@gmail.com>
2023-05-17 18:42:34 -0600

    Update

--- firmware/stepper_motor/stepper_motor.ino
+++ firmware/stepper_motor/stepper_motor.ino
@@ -7,10 +7,10 @@ const int stepsPerRevolution = 2048;
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
 Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
-int stepsPerLetter = -53;
+int stepsPerLetter = -51;
 
 void setup() {
-    myStepper.setSpeed(10);
+    myStepper.setSpeed(15);
 }
 
 void moveLetters(int letters) {
@@ -18,7 +18,55 @@ void moveLetters(int letters) {
 }
 
 void loop() {
+  delay(5000);
+  moveLetters(25);
   delay(500);
-  // Rotate CCW quickly at 10 RPM
-  moveLetters(1);
+  moveLetters(13);
+  delay(500);
+  moveLetters(37);
+  delay(5000);
+  moveLetters(6);
 }
+
+/*
+ * a
+ * b
+ * c
+ * d
+ * e
+ * f
+ * g
+ * h
+ * i
+ * j
+ * k
+ * l
+ * m
+ * n
+ * o
+ * p
+ * q
+ * r
+ * s
+ * t
+ * u
+ * v
+ * w
+ * x
+ * y
+ * z
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * 0
+ * ?
+ * !
+ * .
+ *  
+ */
